a18c938174871116f9e7d4f51b944093a1bc5859
Estherevellyn <estherevellyn@gmail.com>
2023-10-04 00:42:38 -0300

    projeto em andamento

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let sections = document.querySelectorAll('section');
 
+let sections = document.querySelectorAll('section');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
@@ -9,20 +9,49 @@ window.onscroll = () => {
         if (top >= offset && top < offset + height) {
             sec.classList.add('show-animate');    
         }
-
         else {
             sec.classList.remove('show-animate');
         }
     })
 }
 
-var swiper = new Swiper('.swiper-container', {
-    direction: 'vertical',
-    sliderPerView: 1,
-    spaceBetween: 0,
-    mousewheel: true,
-    pagination: {
-        el:'.swiper-pagination',
-        type: 'progressbar',
+const sliderContainer = document.querySelector('.slider-container');
+
+const slideRight = document.querySelector('.right-slide');
+const slideLeft = document.querySelector('.left-slide');
+
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+
+const slidesLength = slideLeft.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength -1) * 100}vh`;
+
+
+//adicionar eventlineart no buttons 
+
+upButton.addEventListener('click', () => changeSlide('up'));
+downButton.addEventListener('click', () => changeSlide('down'));
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight;
+    if(direction == 'up'){
+    activeSlideIndex++;
+    if(activeSlideIndex > slidesLength -1){
+    activeSlideIndex = 0;
     }
-}) 
+    } else if (direction === 'down'){
+        activeSlideIndex--;
+        if(activeSlideIndex < 0){
+         activeSlideIndex = slidesLength -1;   
+        }
+    }
+
+slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
+slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
+};
+
+
+
